99b4f2f8f3652bc8b8f263f0c3d887d167155b25
rafemo1718daw2 <root@daw2m08.fjeclot.net>
2018-01-31 17:04:48 +0100

    Modificació 1 de la prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -26,6 +26,7 @@ public class Jenkins {
             bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
+			bw.write("    Modificació del projecte jenkins");bw.newLine();
             bw.close();
         }
     }
